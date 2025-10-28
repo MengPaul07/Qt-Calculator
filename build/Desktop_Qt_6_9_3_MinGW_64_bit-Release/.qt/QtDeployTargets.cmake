415db316c1fb345ae20d9c93e2c3e571039e00b2
MengPaul <mengpao123@126.com>
2025-10-28 22:26:50 +0800

    Release

---
+++ build/Desktop_Qt_6_9_3_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Calculator_FILE C:/Users/mengp/Desktop/Repository/Calculator/build/Desktop_Qt_6_9_3_MinGW_64_bit-Release/Calculator.exe)
+set(__QT_DEPLOY_TARGET_Calculator_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Calculator_RUNTIME_DLLS D:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
